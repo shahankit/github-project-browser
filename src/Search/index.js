f617417054ca272fb204429f9cbc3f3dce4fa2ea
Ankit Shah <shahankit2313@gmail.com>
2017-07-15 20:29:31 +0530

    Added search screen and search bar

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import {
   View,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
+  },
+  searchBarWrapper: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+  },
+  searchBar: {
+    flex: 1
+  },
+  searchButton: {
+    width: 60,
+    alignSelf: 'stretch',
+    backgroundColor: '#e1e8ee',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
@@ -18,7 +35,22 @@ class Search extends Component {
 
   render() {
     return (
-      <View style={styles.container} />
+      <View style={styles.container}>
+        <View style={styles.searchBarWrapper}>
+          <SearchBar
+            containerStyle={styles.searchBar}
+            lightTheme={true}
+            noIcon={true}
+            onChangeText={this.onChangeText}
+            placeholder={'Enter search text...'}
+            defaultValue={this.props.navigation.state.params.searchString}
+            clearIcon={{ color: '#86939e', name: 'clear' }}
+          />
+          <TouchableOpacity style={styles.searchButton}>
+            <Text>Search</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
